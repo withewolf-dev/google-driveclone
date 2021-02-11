558bb1e321a3e0be203fd9ac18719f02c1fe1349
withewolf-dev <itskashyap26@gmail.com>
2021-02-11 12:23:53 +0530

    upload function and getdownload link

--- components/button/upload-button.js
+++ components/button/upload-button.js
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
-import { Platform, View } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Alert, Platform, Text, View } from 'react-native'
 import { IconButton,Colors } from 'react-native-paper'
 import storage from '@react-native-firebase/storage';
-import { utils,firebase} from '@react-native-firebase/app';
 import DocumentPicker from 'react-native-document-picker';
-import RNFetchBlob from 'rn-fetch-blob'
+import firestore from '@react-native-firebase/firestore';
+import { GlobalContext } from '../../Global/Global-state';
+import auth from '@react-native-firebase/auth';
+
+var RNFS = require('react-native-fs');
 
 export  function UploadButton() {
 
-  const [File, setFile] = useState(null)
-  const [Uploading, setUploading] = useState(false)
-  const [Transferred, setTransferred] = useState(0)
 
 
-   const testing = async()=>{
+
+   const FileUpload = async()=>{
     try {
       const res = await DocumentPicker.pick({
         type: [DocumentPicker.types.allFiles],
       });
      
+  
+ 
+      const file = await RNFS.readFile(res.uri, 'base64')
+      console.log(file,"file3");
+     
+      const ref = storage().ref(res.name)
+      const task = await ref.putString(file,"base64",{contentType:res.type})
+      const link = await ref.getDownloadURL()
 
-      const task = storage()
-      .ref('testing')
-      .putFile(res.uri)
+      console.log(task,"task");
+      console.log(link,"link");
 
-      console.log(decodeURIComponent(res.uri));
-
-      task.on('state_changed', snapshot => {
-        setTransferred(
-          Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000
-        );
-      });
-      try {
-        await task;
-      } catch (e) {
-        console.error(e);
-      }
-      setUploading(false);
+      firestore().collection('links').add({
+        link:link,
+        uid:auth().currentUser.uid
+      }).then((value)=>{
+        console.log("added to firestore");
+      }).catch((e)=>{
+        console.log(e);
+      })
+      
+      //setUploading(false);
       Alert.alert(
         'File uploaded!',
         'Your File has been uploaded to Firebase Cloud Storage!'
       );
 
-    } catch (err) {
+    } 
+    
+    catch (err) {
       if (DocumentPicker.isCancel(err)) {
         // User cancelled the picker, exit any dialogs or menus and move on
       } else {
@@ -55,7 +62,6 @@ export  function UploadButton() {
 
 
 
-   
     return (
         <View>
              <IconButton
@@ -63,7 +69,7 @@ export  function UploadButton() {
             color={Colors.red500}
             size={30}
         
-           onPress={testing}
+           onPress={FileUpload}
           />
         </View>
     )
